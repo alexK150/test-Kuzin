2f518745a2c13338f400f6329a918b14706fb813
git4res <akuzin20@gmail.com>
2022-09-10 16:23:54 +0300

    header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,28 +7,37 @@ const Header = () => {
             id: 1,
             skill: 'PHP',
             years: 3
+        },
+        {
+            id: 2,
+            skill: 'JavaScript',
+            years: 3
         }
     ]
 
     return(
         <header className="header">
-            <main className="header__user">
-                <img src="/assets/img/userpic.svg" className="header__user--picture" alt="User picture"/>
-                <div className="header__user--info">
-                    <h1 className="name">John Smith</h1>
-                    <h4 className="from">Portland, Oregon, USA</h4>
-                    <h4 className="language"><span className="flag"/> English</h4>
-                    <section className="skills">
-                        {
-                            skills.map(el => <div className="skill" key={el.id}>{el.skill}</div>)
-                        }
-                        <button className="skills-btn">+</button>
-                    </section>
+            <div className="header__container">
+                <main className="header__user">
+                    <img src="/assets/img/userpic.svg" className="header__user--picture" alt="User picture"/>
+                    <div className="header__user--info">
+                        <h1 className="name">John Smith</h1>
+                        <h4 className="from">Portland, Oregon, USA</h4>
+                        <h4 className="language"><span className="flag"/><span className="eng">English</span></h4>
+                        <section className="skills">
+                            {
+                                skills.map(el => <div className="skill" key={el.id}>{el.skill}</div>)
+                            }
+                            <button className="skills-btn">
+                                <img src="/assets/icons/ic-add.svg" alt="add button"/>
+                            </button>
+                        </section>
+                    </div>
+                </main>
+                <div className="header__print">
+                    <span className="printer"/>
+                    <div className="print-text">Print this page</div>
                 </div>
-            </main>
-            <div className="header__link">
-                <span className="printer"></span>
-                <a href="#">Print this page</a>
             </div>
         </header>
     )
